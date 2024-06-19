e5d532baf7898748d1ff20d59630f9339dad1e9c
ComsenseInternalProjects <suvakantar@comsenseconsulting.com>
2024-06-19 16:36:20 +0530

    Suva portfolio added

---
+++ data/index.ts
@@ -0,0 +1,196 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I Prioritize Client Collaboration and Promoting Transparency",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I adapt to diverse time zones for communication",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Tech Stack",
+    description: "Updated with latest technologies",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Creating an AI tool for CEOs to manage content generation",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/code.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Created a RAG Product using Llama-index",
+    des: "Developed a Retrieval-Augmented Generation (RAG) product leveraging Llama-index, an open-source library for building question-answering systems",
+    img: "/rag_llama.png",
+    iconLists: ["/python.svg", "/openai.png", "/docker.png", "/hf-logo.png", "/rag.svg"],
+    link: "https://github.com/Suvakanta8",
+  },
+  {
+    id: 2,
+    title: "SQL Query Builder from Natural language",
+    des: "An AI-powered system that generates SQL queries directly from natural language input, leveraging OpenAI's language model and a custom-trained model for accurate translation.",
+    img: "/text2sql.svg",
+    iconLists: ["/python.svg", "/openai.png", "/docker.png", "/hf-logo.png", "/rag.svg"],
+    link: "https://github.com/Suvakanta8",
+  },
+  {
+    id: 3,
+    title: "Competitor Monitoring AI Tool​",
+    des: "To provide timely updates on competitors' activities by monitoring their news, projects, and relevant articles from Google News.​",
+    img: "/monitoring.svg",
+    iconLists: ["/python.svg", "/openai.png", "/docker.png", "/hf-logo.png", "/rag.svg"],
+    link: "https://github.com/Suvakanta8",
+  },
+  {
+    id: 4,
+    title: "Improve E-commerce Seller Operations with AI",
+    des: "Engineered an e-commerce AI platform using generative AI and Langchain for improved seller operations",
+    img: "/ecom.svg",
+    iconLists: ["/python.svg", "/openai.png", "/docker.png", "/hf-logo.png", "/rag.svg"],
+    link: "https://github.com/Suvakanta8",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Suvakanta was a game-changer for my project. This AI work was transformative for my investment strategy. They built an AI tool that sifts through financial news and data, delivering actionable insights in real-time. It's given me a significant edge, allowing me to make informed stock market decisions faster than ever. Their AI expertise has been invaluable.",
+    name: "Manoj Kumar Nayak",
+    title: "Investment Analyst",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Data Scientist - NLP Specialist",
+    desc: "Experienced NLP specialist using AI-driven techniques to derive insights and enhance language systems",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Software Engineer - AI/ML",
+    desc: "Innovative AI/ML engineer crafting algorithms to automate processes and drive data-driven solutions",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance GEN AI Engineer",
+    desc: "Innovative freelance Gen AI engineer building cutting-edge generative AI solutions using the latest AI technologies for diverse client needs",
+    className: "md:col-span-full md:flex md:justify-center", 
+    thumbnail: "/exp3.svg",
+  },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Suvakanta8",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "/",
+  },
+];
